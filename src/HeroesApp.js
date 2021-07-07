14a88533c77962c6ff8f9fbdb83962c632cf1334
Ronald Blanco <ronaldblanco509@gmail.com>
2021-07-06 22:36:19 -0400

    Inicio de la App

---
+++ src/HeroesApp.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HeroesApp = () => {
+    return (
+        <div>
+            <h1>HeroesApp</h1>
+        </div>
+    );
+};
+
+HeroesApp.propTypes = {
+
+};
+
+export default HeroesApp;
